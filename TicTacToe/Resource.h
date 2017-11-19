6d30d094877a17dc8def0b80d69cfe3c923ab925
Tomasz Leman <apacz87@gmail.com>
2017-11-19 14:14:56 +0100

    Add New Game into main window menu

--- TicTacToe/Resource.h
+++ TicTacToe/Resource.h
@@ -10,6 +10,7 @@
 #define IDD_ABOUTBOX            103
 #define IDM_ABOUT               104
 #define IDM_EXIT                105
+#define IDM_NEWGAME             106
 #define IDI_TICTACTOE           107
 #define IDI_SMALL               108
 #define IDC_TICTACTOE           109
